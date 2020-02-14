e4e281bd0bbdb083999cb34049627f32d66ad2d4
chuckki <chuckki@projektorientiert.de>
2020-02-14 10:54:36 +0100

    initial

---
+++ src/ChuckkiMilbonBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Chuckki\MilbonBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ChuckkiMilbonBundle extends Bundle
+{
+}
